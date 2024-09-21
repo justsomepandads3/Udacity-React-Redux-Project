6ff32f92d0b57b1cf4e25584dfe7c01fc3eca789
justsomepandas3 <ninetom16@gmail.com>
2024-09-21 14:49:30 +0400

    Updates 1

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,7 @@
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { connect } from "react-redux";
-
+import { useEffect } from "react";
+import avatarDefault from "../utils/profile-default-svgrepo.svg"
 const withRouter = (Component) => {
     const ComponentWithRouterProp = (props) => {
       let location = useLocation();
@@ -9,43 +10,46 @@ const withRouter = (Component) => {
       return <Component {...props} router={{ location, navigate, params }} />;
     };
     return ComponentWithRouterProp;
-  };
+};
+
+
 
 const Question = (props) =>{
     let isDone = false
+  
     
-    console.log(props.author)
-    if(props.optionOne.includes(props.authedUser) || props.optionTwo.includes(props.authedUser))
+    if(props.optionsVotes.includes(props.authedUser))
         isDone = true;
 
 
     return(
         <div>
             {isDone ?   
-                <div className="question-container">
+                <div className="question-page-container">
                    
 
                 </div>
                 
                 :
             
-                <div className="question-container">
-                    <h1 className="question-creator">Poll by {props.author}</h1>
-                    <img className="question-creator-img"></img>
+                <div className="question-page-container">
+                    <h1 className="question-page-creator">Poll by {props.author}</h1>
+                    <img className="question-page-creator-img"></img>
                     
-                    <div className="question">
+                    <div className="question-page">
                         <h1 >Would You Rather</h1>
-                        <div className="question-options">
-                            <div className="question-option">
-                                <p>jj{}</p>
+                        <div className="question-page-options">
+                            <div className="question-page-option">
+                                <p>Write your question here to submit it to the react app yikkes!</p>
                                 <button>clickme</button>
                             </div>
-                            <div className="question-option">
-                                <p>ss{}</p>
+                            <div className="question-page-option">
+                                <p>Write your question here to submit it to the react app yikkes!</p>
                                 <button>clickme</button>
                             </div>
                         </div>
                     </div>
+
                 </div> 
             }
         </div>
@@ -55,16 +59,20 @@ const Question = (props) =>{
 const mapStateToProps = ({users, questions, authedUser}, props) =>{
     const { id } = props.router.params;
     const question = Object.values(questions).filter(question => question.id===id);
-    const author = question.author;
+    
+    const author = question[0].author;
     const optionOne =questions[id].optionOne.votes;
     const optionTwo =questions[id].optionTwo.votes;
+    const optionsVotes = []
+    optionsVotes.concat(optionOne,optionTwo)
+    const userAvatar= Object.values(users).filter(user=> user.id===question.author);
 
     return{
         question,
         author,
         authedUser,
-        optionOne,
-        optionTwo,
+        optionsVotes,
+       
 
     }
 }
